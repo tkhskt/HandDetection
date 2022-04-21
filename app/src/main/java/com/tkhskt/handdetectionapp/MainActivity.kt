7a053821a85016cff102f8eb4d971188cb6624c9
tkhskt <luckymortality@gmail.com>
2022-04-22 02:47:33 +0900

    hand depth

--- app/src/main/java/com/tkhskt/handdetectionapp/MainActivity.kt
+++ app/src/main/java/com/tkhskt/handdetectionapp/MainActivity.kt
@@ -1,11 +1,215 @@
 package com.tkhskt.handdetectionapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.FrameLayout
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import com.google.mediapipe.framework.TextureFrame
+import com.google.mediapipe.solutioncore.CameraInput
+import com.google.mediapipe.solutioncore.SolutionGlSurfaceView
+import com.google.mediapipe.solutioncore.VideoInput
+import com.google.mediapipe.solutions.hands.HandLandmark
+import com.google.mediapipe.solutions.hands.Hands
+import com.google.mediapipe.solutions.hands.HandsOptions
+import com.google.mediapipe.solutions.hands.HandsResult
 
+@RequiresApi(Build.VERSION_CODES.N)
 class MainActivity : AppCompatActivity() {
+    private val TAG = "MainActivity"
+
+    private var hands: Hands? = null
+
+    // Run the pipeline and the model inference on GPU or CPU.
+    private val RUN_ON_GPU = true
+
+    private enum class InputSource {
+        UNKNOWN, IMAGE, VIDEO, CAMERA
+    }
+
+    private var inputSource = InputSource.UNKNOWN
+
+    private var imageView: HandsResultImageView? = null
+
+    // Video demo UI and video loader components.
+    private var videoInput: VideoInput? = null
+
+    // Live camera demo UI and camera components.
+    private var cameraInput: CameraInput? = null
+
+    private var glSurfaceView: SolutionGlSurfaceView<HandsResult>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        setupStaticImageDemoUiComponents()
+//        setupVideoDemoUiComponents()
+        setupLiveDemoUiComponents()
+        imageView = HandsResultImageView(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (inputSource == InputSource.CAMERA) {
+            // Restarts the camera and the opengl surface rendering.
+            cameraInput = CameraInput(this)
+            cameraInput!!.setNewFrameListener { textureFrame: TextureFrame? ->
+                hands!!.send(
+                    textureFrame
+                )
+            }
+            glSurfaceView!!.post { startCamera() }
+            glSurfaceView!!.setVisibility(View.VISIBLE)
+        } else if (inputSource == InputSource.VIDEO) {
+            videoInput!!.resume()
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        if (inputSource == InputSource.CAMERA) {
+            glSurfaceView!!.visibility = View.GONE
+            cameraInput!!.close()
+        } else if (inputSource == InputSource.VIDEO) {
+            videoInput!!.pause()
+        }
+    }
+
+    /** Sets up the UI components for the live demo with camera input.  */
+    private fun setupLiveDemoUiComponents() {
+        val startCameraButton: Button = findViewById(R.id.button_start_camera)
+        startCameraButton.setOnClickListener { v ->
+            if (inputSource == InputSource.CAMERA) {
+                return@setOnClickListener
+            }
+            stopCurrentPipeline()
+            setupStreamingModePipeline(InputSource.CAMERA)
+        }
+    }
+
+    /** Sets up core workflow for streaming mode.  */
+    private fun setupStreamingModePipeline(inputSource: InputSource) {
+        this.inputSource = inputSource
+        // Initializes a new MediaPipe Hands solution instance in the streaming mode.
+        hands = Hands(
+            this,
+            HandsOptions.builder()
+                .setStaticImageMode(false)
+                .setMaxNumHands(2)
+                .setRunOnGpu(RUN_ON_GPU)
+                .build()
+        )
+        hands!!.setErrorListener { message: String, e: RuntimeException? ->
+            Log.e(
+                TAG,
+                "MediaPipe Hands error:$message"
+            )
+        }
+        if (inputSource == InputSource.CAMERA) {
+            cameraInput = CameraInput(this)
+            cameraInput!!.setNewFrameListener { textureFrame: TextureFrame? ->
+                hands!!.send(
+                    textureFrame
+                )
+            }
+        } else if (inputSource == InputSource.VIDEO) {
+            videoInput = VideoInput(this)
+            videoInput!!.setNewFrameListener { textureFrame: TextureFrame? ->
+                hands!!.send(
+                    textureFrame
+                )
+            }
+        }
+
+        // Initializes a new Gl surface view with a user-defined HandsResultGlRenderer.
+        glSurfaceView = SolutionGlSurfaceView(this, hands!!.glContext, hands!!.glMajorVersion)
+        glSurfaceView!!.setSolutionResultRenderer(HandsResultGlRenderer())
+        glSurfaceView!!.setRenderInputImage(true)
+        hands!!.setResultListener { handsResult: HandsResult ->
+            logWristLandmark(handsResult,  /*showPixelValues=*/false)
+            glSurfaceView!!.setRenderData(handsResult)
+            glSurfaceView!!.requestRender()
+        }
+
+        // The runnable to start camera after the gl surface view is attached.
+        // For video input source, videoInput.start() will be called when the video uri is available.
+        if (inputSource == InputSource.CAMERA) {
+            glSurfaceView!!.post { startCamera() }
+        }
+
+        // Updates the preview layout.
+        val frameLayout = findViewById<FrameLayout>(R.id.preview_display_layout)
+        imageView!!.visibility = View.GONE
+        frameLayout.removeAllViewsInLayout()
+        frameLayout.addView(glSurfaceView)
+        glSurfaceView!!.visibility = View.VISIBLE
+        frameLayout.requestLayout()
+    }
+
+    private fun startCamera() {
+        cameraInput!!.start(
+            this,
+            hands!!.glContext,
+            CameraInput.CameraFacing.FRONT,
+            glSurfaceView!!.width,
+            glSurfaceView!!.height
+        )
+    }
+
+    private fun stopCurrentPipeline() {
+        if (cameraInput != null) {
+            cameraInput!!.setNewFrameListener(null)
+            cameraInput!!.close()
+        }
+        if (videoInput != null) {
+            videoInput!!.setNewFrameListener(null)
+            videoInput!!.close()
+        }
+        if (glSurfaceView != null) {
+            glSurfaceView!!.visibility = View.GONE
+        }
+        if (hands != null) {
+            hands!!.close()
+        }
+    }
+
+    private fun logWristLandmark(result: HandsResult, showPixelValues: Boolean) {
+        if (result.multiHandLandmarks().isEmpty()) {
+            return
+        }
+        val wristLandmark = result.multiHandLandmarks()[0].landmarkList[HandLandmark.WRIST]
+        // For Bitmaps, show the pixel values. For texture inputs, show the normalized coordinates.
+        if (showPixelValues) {
+            val width = result.inputBitmap().width
+            val height = result.inputBitmap().height
+            Log.i(
+                TAG, String.format(
+                    "MediaPipe Hand wrist coordinates (pixel values): x=%f, y=%f",
+                    wristLandmark.x * width, wristLandmark.y * height
+                )
+            )
+        } else {
+            Log.i(
+                TAG, String.format(
+                    "MediaPipe Hand wrist normalized coordinates (value range: [0, 1]): x=%f, y=%f",
+                    wristLandmark.x, wristLandmark.y
+                )
+            )
+        }
+        if (result.multiHandWorldLandmarks().isEmpty()) {
+            return
+        }
+        val wristWorldLandmark =
+            result.multiHandLandmarks()[0].landmarkList[HandLandmark.WRIST]
+        Log.i(
+            TAG, String.format(
+                "MediaPipe Hand wrist world coordinates (in meters with the origin at the hand's"
+                        + " approximate geometric center): x=%f m, y=%f m, z=%f m",
+                wristWorldLandmark.x, wristWorldLandmark.y, wristWorldLandmark.z
+            )
+        )
     }
 }
